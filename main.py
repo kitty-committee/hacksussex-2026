92ffaae0547a62b23096e19c8487b8c4edbc3daf
josephholland07 <joeholland2107@gmail.com>
2026-02-28 13:57:15 +0000

    made toggle function
    saves if the light is on or not to a global state so that it knows if it is on or not

--- main.py
+++ main.py
@@ -4,17 +4,30 @@ pin = Pin("LED", Pin.OUT)
 
 # Create a Pin object named "buzzer" connected to GPIO pin 16 as an output
 
+light = Pin(9, Pin.OUT)
 buzzer = Pin(12, Pin.OUT)
+toggled = False
+
+def toggle():
+    global toggled
+    if(toggled):
+        light.off()
+        buzzer.low()
+        toggled = False
+    else:
+        light.on()
+        buzzer.high()
+        toggled = True
 
 
 
 while True:
 
   # Turn the buzzer on
-  buzzer.high()
+  toggle()
   # Pause the program execution for 1 second
   sleep(1)
   # Turn the buzzer off
-  buzzer.low()
+  toggle()
   # Pause the program execution for 1 second
   sleep(1)
